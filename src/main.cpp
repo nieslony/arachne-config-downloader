86db9dafb6ababe287de6ae65bba213b0d8d3d9c
Claas Nieslony <bb@nieslony.at>
2017-04-09 10:18:10 +0200

    As generated by Netbeans

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: claas
+ *
+ * Created on 09. April 2017, 09:57
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
