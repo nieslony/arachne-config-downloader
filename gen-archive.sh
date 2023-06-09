07e4a36426a122bab11ebc43c66ee76f6966dc4d
Claas Nieslony <bb@nieslony.at>
2023-06-09 21:15:41 +0200

    Print status

--- gen-archive.sh
+++ gen-archive.sh
@@ -3,7 +3,9 @@
 VERSION=0.1.0
 PACKAGE=ArachneConfigDownloader
 PKG_VERSION="$PACKAGE-$VERSION"
+TAR_FILE=$PKG_VERSION.tar.gz
 
+echo Creating ${TAR_FILE}...
 git archive \
     --format=tar.gz \
     --prefix $PKG_VERSION/ \
